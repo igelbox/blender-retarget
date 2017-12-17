3c1a7dda9c996526354d213d80ab702d1b62cc26
Vakhurin Sergey <igelbox@gmail.com>
2017-12-17 07:43:02 +0300

    v0.1.0

--- animation_retarget/__init__.py
+++ animation_retarget/__init__.py
@@ -1,15 +1,13 @@
 bl_info = {
     'name':     'Animation Retargeting Tools',
     'author':   'Vakhurin Sergey (igel)',
-    'version':  (0, 0, 1),
+    'version':  (0, 1, 0),
     'blender':  (2, 77, 0),
     'category': 'Animation',
     'location': 'Properties > Object, Properties > Bone',
     'description': 'Applies an animation from one armature to another',
     'wiki_url': 'https://github.com/igelbox/blender-retarget',
     'tracker_url': 'https://github.com/igelbox/blender-retarget/issues',
-    'warning':  'This is just a proof of concept version. \
-Further versions may be not compatible with settings from this version.'
 }
 
 
