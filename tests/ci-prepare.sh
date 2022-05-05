f545057fc3bf0d0f1a0aa5517ff7f91eedce3dfe
Sergei Vakhurin <igelbox@gmail.com>
2022-05-05 23:35:04 +0300

    upgrade coverage to 6.3.2

--- tests/ci-prepare.sh
+++ tests/ci-prepare.sh
@@ -14,11 +14,11 @@ if [ ! -e "$BLENDER_PATH" ]; then
     mkdir -p $TGT
     ln -s tests $TGT/
 
-    URL="https://pypi.python.org/packages/53/fe/9e0fbdbca15c2c1253379c3a694f4315a420555e7874445b06edeaeacaea/coverage-4.2.tar.gz#md5=1e09362a3f35d589f942359441050a6e"
+    URL="https://files.pythonhosted.org/packages/74/fb/f481628033d42f6f6021af8a9a13d913707221e139567f39b09b337421b9/coverage-6.3.2.tar.gz"
     TGT="$BLENDER_PATH/$VER/python/lib/python$BLENDER_PYTHON_VERSION/"
     echo "Downloading $URL to $TGT ..."
     test -d "$TGT" # assert the target is exist
     curl -L "$URL" | tar -xz
-    mv coverage-4.2/coverage "$TGT"
-    rm -rf coverage-4.2
+    mv coverage-6.3.2/coverage "$TGT"
+    rm -rf coverage-6.3.2
 fi
