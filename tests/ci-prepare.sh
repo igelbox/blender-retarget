50a1349f9ea4b4c8efbc4b82f64696652c8ee11a
Sergei Vakhurin <igelbox@gmail.com>
2022-05-05 22:57:27 +0300

    assert wrong python

--- tests/ci-prepare.sh
+++ tests/ci-prepare.sh
@@ -17,6 +17,7 @@ if [ ! -e "$BLENDER_PATH" ]; then
     URL="https://pypi.python.org/packages/53/fe/9e0fbdbca15c2c1253379c3a694f4315a420555e7874445b06edeaeacaea/coverage-4.2.tar.gz#md5=1e09362a3f35d589f942359441050a6e"
     TGT="$BLENDER_PATH/$VER/python/lib/python$BLENDER_PYTHON_VERSION/"
     echo "Downloading $URL to $TGT ..."
+    test -d "$TGT" # assert the target is exist
     curl -L "$URL" | tar -xz
     mv coverage-4.2/coverage "$TGT"
     rm -rf coverage-4.2
